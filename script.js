081881a615d895b5af5a9c03cb695bf3ccd5c897
nchiorsac <natasha@okapps.net>
2022-11-20 13:19:57 +1100

    added functionality to calculator

--- script.js
+++ script.js
@@ -1,54 +1,139 @@
+let previousOperand = '';
+let currentOperand = '';
+let operator = '';
+let hasPressedEquals = false;
+
+const display = document.getElementById('display');
+const clearButton = document.getElementById('clear');
+const clearAllButton = document.getElementById('clear-all');
+const changeSignButton = document.getElementById('change-sign');
+const equalsButton = document.querySelector('.equals');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+
+clearButton.addEventListener('click', () => clear());
+clearAllButton.addEventListener('click', () => clearAll());
+changeSignButton.addEventListener('click', () => changeSign());
+equalsButton.addEventListener('click', () => equalsButtonAction());
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+);
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', function(event) {
+    setOperator(this.id);
+  } 
+));
+
 function add(a, b){
-    return a + b;
+    return a+b;
 }
 
 function subtract(a, b){
-    return a - b;
+    return a-b;
 }
 
 function multiply(a, b){
-    return a * b;
+    return a*b;
 }
 
 function divide(a, b){
     if (b==0){
-        return "Fool, you cannot divide by zero!";
+        alert("Fool, you cannot divide by zero!");
+        return 0;
     }
-    return a / b;
-}
-
-function power(a, b){
-    return Math.pow(a, b);
+    return a/b;
 }
 
 function operate(operator, a, b){
+    let result = 0;
     switch (operator){
-        case "+":
-            Math.round(add(a, b));
+        case '+':
+            result = add(a, b);
             break;
-        case "-":
-            Math.round(subtract(a, b));
+        case '-':
+            result = subtract(a, b);
             break;
-        case "*":
-            Math.round(multiply(a, b));
+        case '*':
+            result = multiply(a, b);
             break;
-        case "/":
-            Math.round(divide(a, b));
-            break;
-        case "^":
-            Math.round(power(a, b));
+        case '/':
+            result = divide(a, b);
             break;
     }
+    
+    return result;
 }
 
-function display(){
+function setOperator(id){
+    if (previousOperand != ''){
+        returnResult();
+    }
+    operator = id;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function appendNumber(number){
+    if (currentOperand.length>=15){
+        return;
+    }
+    if (number == '.' && currentOperand.includes('.')){
+        return;
+    }
+    if (hasPressedEquals && operator == ''){
+        previousOperand = currentOperand;
+        currentOperand = number;
+        display.textContent = number;
+        hasPressedEquals = false;
+    } else {
+        currentOperand += number;
+        display.textContent = currentOperand;
+    }
+
+}
+
+function changeSign(){
+    currentOperand = (-Number(currentOperand)).toString();
+    display.textContent = currentOperand;
+}
+
+function returnResult(){
+    if (operator == ''){
+        return;
+    }
+    let result = operate(operator, Number(previousOperand), Number(currentOperand)).toString();
+    if (result.length>=15){
+        result = result.slice(0, 15);
+    }
+    display.textContent = result;
+    currentOperand = result;
+    operator = '';
+}
+
+function equalsButtonAction(){
+    returnResult();
+    hasPressedEquals = true;
+}
+
+function resetDisplay(){
+    currentOperand = '';
+    display.textContent = '0';
 
 }
 
 function clearAll(){
-
+    resetDisplay();
+    previousOperand = '';
 }
 
 function clear(){
-
+    if (currentOperand=='Infinity'){
+        resetDisplay();
+    }
+    else if (currentOperand.length <= 1){
+        resetDisplay();
+    } else {
+        currentOperand = currentOperand.slice(0, -1);
+        display.textContent = currentOperand;
+    }
 }
